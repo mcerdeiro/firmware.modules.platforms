32684434bf811490dfd403a5f752e3a91d4c5c41
Mariano Cerdeiro <mcerdeiro@gmail.com>
2014-04-24 06:27:51 +0200

    reorg rtos and rname patforms to platforms

---
+++ cortexM4/lpc4337/bsp/src/ciaaUART.c
@@ -0,0 +1,27 @@
+
+#include "ciaaUART.h"
+
+int32_t ciaaUART_open (const char* pathName, int32_t flags)
+{
+	// TODO
+}
+
+int32_t ciaaUART_close (int32_t fd)
+{
+	// TODO
+}
+
+int32_t ciaaUART_ioctl (int32_t fd, int32_t arg, uint32_t size)
+{
+	// TODO
+}
+
+int32_t ciaaUART_read (int32_t fd, uint8_t* buffer, uint32_t size)
+{
+	// TODO
+}
+
+int32_t ciaaUART_write (int32_t fd, uint8_t* buffer, uint32_t size)
+{
+
+}
